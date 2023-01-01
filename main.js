559a420acfee08f9fd362d76059c9266ba16c810
mahmudul607 <111509406+mahmudul607@users.noreply.github.com>
2023-01-01 10:20:06 +0600

    Update work -1

---
+++ main.js
@@ -0,0 +1,56 @@
+const searchGan = () => {
+    const searchText = document.getElementById("search-field").value;
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`
+    //load data
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayGan(data.data))
+    .catch(error => displayError('Something Went Wrong!! Please try again later!'));
+}
+
+const displayGan = songs => {
+    const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
+    songs.forEach(song => {
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
+        songDiv.innerHTML =`
+        <div class="col-md-9">
+        <h3 class="lyrics-name">${song.title}</h3>
+        <p class="author lead">Album by <span>${song.artist.name}</span></p>
+        <audio controls>
+        <source src="${song.preview}" type"audio/mpeg">
+        </audio>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+        <button onclick ="getLyric('${song.artist.name}',${song.title}')" class="btn btn-success">Get Lyrics</button>
+        </div>`;
+        songContainer.appendChild(songDiv);
+
+       
+       
+        
+    })
+}
+
+const getLyric = async (artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        displayLyrics(data.lyrics);
+    }
+    catch (error){
+        displayError('Sorry! I failed to load lyrics, Please try again later!!!')
+    }
+}
+
+const displayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById("song-lyrics");
+    lyricsDiv.innerText = lyrics;
+
+}
+const displayError = error => {
+    const errorTag = document.getElementById('error-message');
+    errorTag.innerText = error;
+}
